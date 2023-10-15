93d1263d3aef000e5a20924fd9d5aaa87cc9f7cc
Orateng <1818.blackmail@gmail.com>
2023-10-15 08:37:53 -0700

    My file

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
 
 class square():
-
+   """Class representing a square."""
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
+        """Instantiation of class attributes."""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
-        """ Area of the square """
+        """Area of the square."""
         return self.width * self.width
 
-    def PermiterOfMySquare(self):
+    def perimiter_of_my_square(self):
+        """Calculates perimeter of the sqaure."""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """Returns a string representation."""
         return "{}/{}".format(self.width, self.height)
 
 
